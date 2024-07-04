faaf01db4344b39b97f12a176f002a83127b61cb
MelleJo <mellejorna@gmail.com>
2024-07-04 10:49:35 +0200

    test

--- .src/smart_analyzer.py
+++ .src/smart_analyzer.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_community import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 import json

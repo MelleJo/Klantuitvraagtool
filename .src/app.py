b1f8b13397a4cb3855de9783708e73969498b7eb
MelleJo <mellejorna@gmail.com>
2024-07-08 14:12:27 +0200

    test

--- .src/app.py
+++ .src/app.py
@@ -10,7 +10,8 @@ st.cache_resource.clear()
 from openai_service import perform_gpt4_operation
 from utils.audio_processing import transcribe_audio, process_audio_input
 from utils.file_processing import process_uploaded_file
-from services.summarization_service import run_klantuitvraag
+from .services.summarization_service import run_klantuitvraag
+#from services.summarization_service import run_klantuitvraag
 from ui.components import display_transcript, display_klantuitvraag
 from ui.pages import render_feedback_form, render_conversation_history
 from utils.text_processing import update_gesprekslog, load_questions
